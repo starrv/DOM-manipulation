b877508d9ac7b306869e21242fdf3f7f6e6da422
Valencia Starr <valenciastarr@Valencias-MacBook-Pro.local>
2023-05-23 12:16:30 -0400

    code

---
+++ index.js
@@ -0,0 +1,34 @@
+// const arr=[2,3,4,5]
+// /* for(let i=0; i<arr.length; i++){
+//     console.log(arr[i])
+// } 
+
+// arr.forEach(item=>console.log(item))
+
+// function add(num1,num2){
+//     return num1+num2
+// }
+
+// console.log(add(2,3))
+// */
+
+function init(){
+    const name=prompt("Please enter your name:")
+    const imageURL=prompt("Please enter your profile picture url:")
+    
+    const p=document.createElement("p")
+    p.textContent=`Welcome ${name}`
+    document.body.append(p)
+
+    const image=document.createElement("img")
+    image.src=imageURL
+    image.width=100
+    image.height=100
+    document.body.appendChild(image)
+
+    document.body.removeChild(document.getElementById("main-heading"))
+}
+
+
+init()
+
